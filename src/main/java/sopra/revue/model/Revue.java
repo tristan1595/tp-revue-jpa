0cbdcfa8f214421a2c4e7f254dacf4cb0ab5af3e
Favrot <tom@>
2021-05-17 16:58:00 +0200

    classe revue

---
+++ src/main/java/sopra/revue/model/Revue.java
@@ -0,0 +1,58 @@
+package sopra.revue.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "magazine")
+
+public class Revue {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(name = "name")
+	private String nom;
+	@Column(name = "periodicity")
+	private Periodicite periodicite;
+	
+	public Revue() {
+		super();
+	}
+	
+	public Revue(String nom, Periodicite periodicite) {
+		super();
+		this.nom = nom;
+		this.periodicite = periodicite;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Periodicite getPeriodicite() {
+		return periodicite;
+	}
+
+	public void setPeriodicite(Periodicite periodicite) {
+		this.periodicite = periodicite;
+	}
+			
+}
