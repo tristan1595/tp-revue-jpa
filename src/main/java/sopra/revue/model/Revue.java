d59fb44b7a2b8f40d31f34fc8b1005281cb62db0
Favrot <tom@>
2021-05-18 09:12:51 +0200

    revue import

--- src/main/java/sopra/revue/model/Revue.java
+++ src/main/java/sopra/revue/model/Revue.java
@@ -1,8 +1,5 @@
 package sopra.revue.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
